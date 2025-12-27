68f7f28da4136472f0a4e7f7debfb85df915d882
Joaquin Cabrera <cabranca@MacBook-Air-de-Joaquin.local>
2025-12-27 13:45:08 -0300

    Add support for MacOS.

--- premake5.lua
+++ premake5.lua
@@ -63,6 +63,37 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
+	filter "system:macosx"
+		pic "On"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"src/cocoa_init.m",
+			"src/cocoa_joystick.m",
+			"src/cocoa_monitor.m",
+			"src/cocoa_window.m",
+			"src/cocoa_time.c",
+			"src/posix_thread.c",
+			"src/nsgl_context.m",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_COCOA" 
+		}
+
+		links
+		{
+			"Cocoa.framework",
+			"IOKit.framework",
+			"CoreFoundation.framework",
+			"CoreVideo.framework"
+		}
+		
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
